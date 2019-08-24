7e4edefdac38190a788cbb016b1406e32c8977af
mrforsyte <petrenko.rostislav@gmail.com>
2019-08-24 22:55:30 +0300

    several simple tests

--- python2/koans/about_asserts.py
+++ python2/koans/about_asserts.py
@@ -27,14 +27,14 @@ class AboutAsserts(Koan):
         """
         Sometimes we will ask you to fill in the values
         """
-        self.assertEqual(__, 1 + 1)
+        self.assertEqual(2, 1 + 1)
 
     def test_assert_equality(self):
         """
         To understand reality, we must compare our expectations against
         reality.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
         self.assertTrue(expected_value == actual_value)
 
@@ -42,7 +42,7 @@ class AboutAsserts(Koan):
         """
         Some ways of asserting equality are better than others.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
 
         self.assertEqual(expected_value, actual_value)
