b218f0163c1058cbd1dcb84f5ef2fbf058a4edbc
mrforsyte <petrenko.rostislav@gmail.com>
2019-11-09 22:34:19 +0300

    poorly solved scoring project, to refactor

--- python3/koans/about_scoring_project.py
+++ python3/koans/about_scoring_project.py
@@ -33,8 +33,23 @@ from runner.koan import *
 # Your goal is to write the score method.
 
 def score(dice):
-    # You need to write this method
-    pass
+    score = 0
+    if dice.count(1) == 3:
+        score+=1000
+    if dice.count(1) == 1:
+        score+=100
+    if dice.count(2) == 3:
+        score += 2*100
+    if dice.count(3) == 3:
+        score += 3*100
+    if dice.count(4) == 3:
+        score += 4*100
+    if dice.count(5) == 3:
+        score += 5*100
+    if dice.count(5) == 1:
+        score += 50
+
+    return score
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
